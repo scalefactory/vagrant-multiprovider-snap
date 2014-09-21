776a5884afa1c7101c827538b74215c793b7eafd
Andrew Stangl <andrewstangl@gmail.com>
2014-09-21 22:20:01 +0200

    add snapshot not deleted messages

---
+++ lib/vagrant-multiprovider-snap/providers/virtualbox/action/message_snapshot_not_deleted.rb
@@ -0,0 +1,27 @@
+module VagrantPlugins
+
+    module ProviderVirtualBox
+
+        module Action
+
+            class MessageSnapshotNotDeleted
+
+                def initialize(app, env)
+                    @app = app
+                end
+
+                def call(env)
+
+                    env[:ui].info I18n.t("vagrant_snap.actions.vm.snapshot_not_deleted")
+
+                    @app.call(env)
+
+                end
+
+            end
+
+        end
+
+    end
+
+end
